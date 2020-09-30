e70c516ead23b81d2d8a08d9999fd8072200d7b4
juatabot <hengjuat@gmail.com>
2020-09-30 11:23:15 -0400

    Add Course service

---
+++ src/services/CourseService.js
@@ -0,0 +1,37 @@
+const url = 'https://wbdv-generic-server.herokuapp.com/api/juat/courses';
+
+const courseService = {
+    findAllCourses: () =>
+        fetch(url)
+            .then(response => response.json()),
+
+    findCourseById: (courseId) =>
+        fetch(`${url}/${courseId}`)
+            .then(response => response.json()),
+
+    deleteCourse: (courseId) =>
+        fetch(`${url}/${courseId}`, {
+            method: "DELETE"
+        })
+            .then(response => response.json()),
+
+    createCourse: (course) =>
+        fetch(url, {
+            method: "POST",
+            body: JSON.stringify(course),
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+            .then(response => response.json()),
+
+    updateCourse: (courseId, updatedCourse) =>
+        fetch(`${url}/${courseId}`, {
+            method: "PUT",
+            body: JSON.stringify(updatedCourse),
+            headers: {
+                "content-type": "application/json"
+            }
+        })
+            .then(response => response.json())
+}
