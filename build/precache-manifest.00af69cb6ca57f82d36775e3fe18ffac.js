6554e19a56ac78932345fb0741c780af11c26107
juatabot <hengjuat@gmail.com>
2020-11-03 16:58:03 -0500

    Herok udeply

---
+++ build/precache-manifest.00af69cb6ca57f82d36775e3fe18ffac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016eff3105ee7b6e285851eeea3f765d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a1a0701c19d191c5982",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7a1a0701c19d191c5982",
+    "url": "/static/js/2.40bf1c88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.40bf1c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b93bc781bc4af89836a",
+    "url": "/static/js/main.f2fa2453.chunk.js"
+  },
+  {
+    "revision": "27914662c8f81eb28d97",
+    "url": "/static/js/runtime-main.89c84c7c.js"
+  }
+]);
